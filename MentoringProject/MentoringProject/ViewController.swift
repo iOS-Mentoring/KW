12cc5517d060ce374a8a6e42ac0462e660c99814
박기우 <KWiOS0101@gmail.com>
2025-02-06 15:02:42 +0900

    [init] 프로젝트 생성

---
+++ MentoringProject/MentoringProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MentoringProject
+//
+//  Created by PKW on 2/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
