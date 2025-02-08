92b260f6bd3a40339a0a316c2feea6cc26cc051d
박기우 <KWiOS0101@gmail.com>
2025-02-08 12:15:24 +0900

    [Feat] 프리뷰 추가

---
+++ MentoringProject/MentoringProject/Utils/Preview.swift
@@ -0,0 +1,27 @@
+//
+//  Preview.swift
+//  MentoringProject
+//
+//  Created by PKW on 2/7/25.
+//
+
+import SwiftUI
+
+#if DEBUG
+extension UIViewController {
+    private struct Preview: UIViewControllerRepresentable {
+        let viewController: UIViewController
+        
+        func makeUIViewController(context: Context) -> UIViewController {
+            return viewController
+        }
+        
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        }
+    }
+    
+    func toPreview() -> some View {
+        Preview(viewController: self)
+    }
+}
+#endif
