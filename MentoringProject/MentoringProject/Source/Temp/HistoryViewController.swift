dfc31f1f6e14519247a020f65761cffad313075f
Ki woo Park <kwios0101@gmail.com>
2025-02-11 01:07:09 +0900

    [Refactor] 히스토리VC 코드 수정

---
+++ MentoringProject/MentoringProject/Source/Temp/HistoryViewController.swift
@@ -0,0 +1,36 @@
+//
+//  TestViewController.swift
+//  MentoringProject
+//
+//  Created by PKW on 2/7/25.
+//
+
+import UIKit
+
+class HistoryViewController: BaseViewController {
+    private let backButton = UIButton()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupUI()
+    }
+}
+
+extension HistoryViewController {
+    func setupUI() {
+        view.backgroundColor = .white
+
+        // 네비게이션 바 백버튼
+        backButton.setImage(.iconLeftArrow, for: .normal)
+        backButton.addTarget(self, action: #selector(backButtonTapped), for: .touchUpInside)
+
+        setLeftBarButtonItem(item: backButton)
+    }
+}
+
+extension HistoryViewController {
+    @objc func backButtonTapped() {
+        navigationController?.popViewController(animated: true)
+    }
+}
