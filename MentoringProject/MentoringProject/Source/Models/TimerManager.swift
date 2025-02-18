6fd3cb81f8c55f930e2e6c88dce069bb9ebc47ea
Ki woo Park <kwios0101@gmail.com>
2025-02-19 04:34:06 +0900

    [Refactor] 타이머매니저 코드 수정

--- MentoringProject/MentoringProject/Source/Models/TimerManager.swift
+++ MentoringProject/MentoringProject/Source/Models/TimerManager.swift
@@ -8,24 +8,43 @@
 import Combine
 import Foundation
 
-final class TimerManager {
-    @Published var elapsedTime: TimeInterval = 0
+protocol TimerManagerProtocol {
+    func start() -> AnyPublisher<Int, Never>
+    func stop()
+    func getCount() -> Int
+}
+
+final class TimerManager: TimerManagerProtocol {
     private var timerCancellable: AnyCancellable?
-
-    func start() {
-        stop()
-        elapsedTime = 0
-
+    private let timerSubject = PassthroughSubject<Int, Never>()
+    private var count = 0
+    private let maxTime = 60
+    
+    func start() -> AnyPublisher<Int, Never> {
+        count = 0 // 카운트 초기화
         timerCancellable = Timer.publish(every: 1, on: .main, in: .common)
             .autoconnect()
-            .sink { [weak self] _ in
+            .prefix(maxTime)
+            .sink(receiveCompletion: { [weak self] _ in
                 guard let self = self else { return }
-                self.elapsedTime += 1
-            }
+                self.stop() // 60초면 타이머 종료
+            }, receiveValue: { [weak self] _ in
+                guard let self = self else { return }
+                self.count += 1 // 타이머 1초씩 증가
+                self.timerSubject.send(self.count) // 증가된 타이머 방출
+            })
+        
+        return timerSubject.eraseToAnyPublisher()
     }
-
+    
     func stop() {
+        print("타이머 종료 합니다.")
         timerCancellable?.cancel()
         timerCancellable = nil
+        timerSubject.send(completion: .finished) // 종료
+    }
+    
+    func getCount() -> Int {
+        return count
     }
 }
