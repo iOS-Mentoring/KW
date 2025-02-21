008e6c3f0cd86845aa95a0b61a2a6963bf902da8
Ki woo Park <kwios0101@gmail.com>
2025-02-21 17:39:01 +0900

    [Chore] 누락된 파일 추가

--- MentoringProject/MentoringProject/Source/Utils/TimerManager.swift
+++ MentoringProject/MentoringProject/Source/Utils/TimerManager.swift
@@ -2,7 +2,7 @@
 //  TimerManager.swift
 //  MentoringProject
 //
-//  Created by PKW on 2/11/25.
+//  Created by PKW on 2/21/25.
 //
 
 import Combine
@@ -19,7 +19,7 @@ final class TimerManager: TimerManagerProtocol {
     private let timerSubject = PassthroughSubject<Int, Never>()
     private var count = 0
     private let maxTime = 60
-    
+
     func start() -> AnyPublisher<Int, Never> {
         count = 0 // 카운트 초기화
         timerCancellable = Timer.publish(every: 1, on: .main, in: .common)
@@ -33,17 +33,17 @@ final class TimerManager: TimerManagerProtocol {
                 self.count += 1 // 타이머 1초씩 증가
                 self.timerSubject.send(self.count) // 증가된 타이머 방출
             })
-        
+
         return timerSubject.eraseToAnyPublisher()
     }
-    
+
     func stop() {
         print("타이머 종료 합니다.")
         timerCancellable?.cancel()
         timerCancellable = nil
         timerSubject.send(completion: .finished) // 종료
     }
-    
+
     func getCount() -> Int {
         return count
     }
--- MentoringProject/MentoringProject/Source/Utils/TypingSpeedCalculator.swift
+++ MentoringProject/MentoringProject/Source/Utils/TypingSpeedCalculator.swift
@@ -2,12 +2,11 @@
 //  TypingSpeedCalculator.swift
 //  MentoringProject
 //
-//  Created by PKW on 2/11/25.
+//  Created by PKW on 2/21/25.
 //
 
 import Foundation
 import UIKit
-
 
 /// 타이핑 속도를 계산하고 입력된 텍스트를 검사하는 클래스 입니다.
 /// - 기능:
@@ -15,7 +14,6 @@ import UIKit
 /// 2. 플레이스홀더 텍스트와 동일한 텍스트만 추출
 /// 3. 입력된 텍스트를 플레이스홀더와 동일한지 비교하고 틀린 텍스트 강조
 final class TypingCalculator {
-
     /// 사용자가 입력한 올바른 텍스트를 기반으로 CPM을 계산합니다.
     /// - Parameters:
     ///   - correctText: 올바르게 입력된 텍스트
@@ -40,8 +38,8 @@ final class TypingCalculator {
     /// - Note:
     /// - 틀린 문자가 나오기 전까지의 문자열을 반환 합니다.
     func getCorrectCharacters(inputText: String, placeholderText: String) -> String {
-        let inputCharacters = Array(inputText)  // 입력한 문자 배열
-        let placeholderCharacters = Array(placeholderText)  // 플레이스홀더 문자 배열
+        let inputCharacters = Array(inputText) // 입력한 문자 배열
+        let placeholderCharacters = Array(placeholderText) // 플레이스홀더 문자 배열
         
         var correctCharacters = ""
 
@@ -49,7 +47,7 @@ final class TypingCalculator {
             if index < placeholderCharacters.count, char == placeholderCharacters[index] {
                 correctCharacters.append(char)
             } else {
-                break  
+                break
             }
         }
         return correctCharacters
