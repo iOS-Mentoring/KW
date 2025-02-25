ac03a5f86589a3944bc3d8d3aff285f11459e870
Ki woo Park <kwios0101@gmail.com>
2025-02-25 20:34:07 +0900

    [Add] 히스토리 테스트용 데이터 추가

--- MentoringProject/MentoringProject/Source/Models/HistoryModel.swift
+++ MentoringProject/MentoringProject/Source/Models/HistoryModel.swift
@@ -8,10 +8,10 @@
 import Foundation
 
 struct HistoryModel {
-    // 날짜
-    // WPM
-    // ACC
-    // 필사 텍스트
-    // 제목
-    // 작가
+    let date: String
+    let wpm: Int
+    let acc: Int
+    let text: String
+    let title: String
+    let author: String
 }
